ccafca10b6c5386ec0907933fc7461dbe38b7e3b
Charu Hire <c.hire@gmail.com>
2021-06-07 00:03:00 +0530

    Automate the Amazon application using Selenium WebDriver with Java.

--- Desktop/JDK/AmazonProject/src/phase1/AutomateAmazon.java
+++ Desktop/JDK/AmazonProject/src/phase1/AutomateAmazon.java
@@ -1,8 +1,9 @@
 package phase1;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -31,7 +32,7 @@ public class  AutomateAmazon{
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
 		driver.get("https://www.amazon.in/");
-	
+		
 		
 		try {
 			    Class.forName("com.mysql.cj.jdbc.Driver");
@@ -42,42 +43,45 @@ public class  AutomateAmazon{
 				
 				while(rs.next()) {
 					
-					System.out.println("Category= "+rs.getString(2)+ " " + "SearchVal= " +rs.getString(3));
+					System.out.println("Category = " +rs.getString(2)+ " " +"SearchVal = "+ rs.getString(3));
 					System.out.println();
 					
 					Category = rs.getString(2);
 					WebElement search = driver.findElement(By.xpath("//select[@id = 'searchDropdownBox']"));
 					search.sendKeys(Category);
 					SearchVal = rs.getString(3);
+					
 					WebElement searchBar = driver.findElement(By.xpath("//input[@id = 'twotabsearchtextbox']"));
 					searchBar.sendKeys(SearchVal);
+					
 					WebElement searchButton = driver.findElement(By.xpath("//input[@type = 'submit']"));
 					searchButton.click();
 					
 					List<WebElement> ShownResults = driver.findElements(By.xpath("//*[@data-component-type='s-search-result']"));
-					System.out.println("Total Number of Shown Results: " + ShownResults.size());   //Total number of mobile items (including sponsored) displayed on amazon searchresult webpage(30).
+					System.out.println("Total Number of Shown Results: " + ShownResults.size()); //Total number of search for mobile including sponsored result(30).
 					System.out.println();
 					
 					List<WebElement> SponsoredResults = driver.findElements(By.xpath("//*[@class = 'aok-inline-block s-sponsored-label-info-icon']"));
-					System.out.println("Total Number of Sponsored Mobiles: " + SponsoredResults.size());   //Total number of sponsored mobile (6).
+					System.out.println("Total Number of Sponsored Mobiles: " + SponsoredResults.size()); //Total number of sponsored mobile result count i.e 6.
 					System.out.println();
 					
 					int Results = ShownResults.size() - SponsoredResults.size();
-					System.out.println("Total Number of Mobiles: " + Results);
+					System.out.println("Total NUmber of Mobiles: " + Results);
 					
 					TakesScreenshot tsObj = (TakesScreenshot)driver;
 					
 					File myFile = tsObj.getScreenshotAs(OutputType.FILE);
 					
-					try {
-				        FileUtils.copyFile(myFile, new File("AmazonSearchList.png"));
-				        }catch(IOException e) {
-				        	//TODO Auto-generated catch block.
-				        	 e.printStackTrace();
-				        }
-					}
+			        try {
+			            FileUtils.copyFile(myFile, new File("tests.png"));
+			            }catch(IOException e) {
+			            	//TODO Auto-generated catch block.
+			            	 e.printStackTrace();
+			            }
+
+				}
 				
-				} catch (SQLException e) {
+			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				
@@ -85,7 +89,6 @@ public class  AutomateAmazon{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		driver.close();
 	}
 
